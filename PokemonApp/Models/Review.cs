b43fc68fd0c29b6acce97962040523377fa9ad30
ameen226 <hosan2662000@gmail.com>
2025-06-17 16:08:18 +0300

    Editing Review Model class

--- PokemonApp/Models/Review.cs
+++ PokemonApp/Models/Review.cs
@@ -5,8 +5,11 @@
         public string Title { get; set; }
         public string Text { get; set; }
         public int Rating { get; set; }
+
         public int ReviewerId { get; set; }
         public Reviewer Reviewer { get; set; }
+
+        public int PokemonId { get; set; }
         public Pokemon Pokemon { get; set; }
     }
 }
